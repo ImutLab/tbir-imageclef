af88e4328cd607132fc3269209bc230799b201e6
Jakub Macina <jakub.macina@gmail.com>
2016-04-16 23:39:02 +0200

    Add averaging with weights

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ from gensim import corpora, models
 from pprint import pprint
 from sklearn.feature_extraction.text import CountVectorizer
 from gensim.models import Word2Vec
+import time
 
 
 images = []
@@ -99,7 +100,7 @@ def loadImageIds(directory, filename):
     # TODO load file with image ids
     print("A")
 
-def makeFeatureVector(words, word_vector_model, num_features):
+def makeFeatureVector(words, word_vector_model, weights, num_features):
     '''
     Function to average all words vectors from a website.
     :param words:
@@ -109,20 +110,23 @@ def makeFeatureVector(words, word_vector_model, num_features):
     # TODO add weights based on score
     feature_vector = np.zeros((num_features,), dtype="float32")
     nwords = 0
+    assert len(words) == len(weights)
     # Index2word is a list that contains the names of the words in
     # the model's vocabulary. Convert it to a set, for speed
     index2word_set = set(word_vector_model.index2word)
-    for word in words:
+    for i in range(0, len(words)):
+        word = words[i]
+        weight = weights[i]
         if word in index2word_set:
             nwords += 1
             # Sum feature vector
-            feature_vector = np.add(feature_vector, word_vector_model[word])
+            feature_vector = np.add(feature_vector, weight*word_vector_model[word])
     # Divide the result by the number of words to get the average
     if(nwords != 0):
         feature_vector = np.divide(feature_vector, nwords)
     return feature_vector
 
-def getAvgFeatureVectors(websites, word_vector_model):
+def getAvgFeatureVectors(websites, word_vector_model, weights):
     '''
     Given a set of website (each one a list of words) calculate the average feature vector
     for each one and return  a 2D numpy array.
@@ -131,18 +135,22 @@ def getAvgFeatureVectors(websites, word_vector_model):
     :return:
     '''
     print("Getting average feature vectors")
+    start = time.time() # Start time
     # Get number of features in word2vec model.
     num_features = word_vector_model.syn0.shape[1]
     # Inicialize a counter.
     counter = 0
     # Preallocate a 2D numpy array, for speed
     website_feature_vecs = np.zeros((len(websites), num_features), dtype="float32")
-    for website in websites:
+    for i in range(0, len(websites)):
+        website = websites[i]
         # Print a status message every 1000th review
         if counter % 1000. == 0 and counter != 0:
            print "Get average vector for %d of %d" % (counter, len(websites))
-        website_feature_vecs[counter] = makeFeatureVector(website, word_vector_model, num_features)
+        website_feature_vecs[counter] = makeFeatureVector(website, word_vector_model, weights[i], num_features)
         counter += 1
+    end = time.time()
+    print "Time taken for averaging feature vectors: ", end-start, "seconds."
     return website_feature_vecs
 
 
@@ -168,6 +176,6 @@ if __name__ == '__main__':
     representAsBow()
     word_vector_model = loadWord2vecModel(word_to_vec_trained_model_data_directory,'vectors.bin')
 
-    examples_average_vectors = getAvgFeatureVectors(examples, word_vector_model)
+    examples_average_vectors = getAvgFeatureVectors(examples, word_vector_model, examples_weights)
     print(examples_average_vectors.shape)
     print(examples_average_vectors[0])
