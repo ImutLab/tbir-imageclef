1ba01cbce22d1b6e59d5896adc2720c4172852db
Jakub Macina <jakub.macina@gmail.com>
2016-04-16 20:40:13 +0200

    Add represetation as bow. Add loading word2vec trained model.

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ from nltk.corpus import stopwords
 from nltk.stem.porter import PorterStemmer
 from gensim import corpora, models
 from pprint import pprint
+from sklearn.feature_extraction.text import CountVectorizer
+from gensim.models import Word2Vec
 
 
 images = []
@@ -37,7 +39,8 @@ def read_textual_features(directory, filename, number_of_examples_to_load):
                 word = line_words[j+2].decode('utf-8')
                 word = p_stemmer.stem(word)
                 weight = float(line_words[j+3]) / 100000
-                if (word not in stop) and (word.isdigit() == False):        # (weight > 2000)   # TODO filter 2 char words
+                # TODO filter 2 char words, better filter digits e.g. 12th
+                if (word not in stop) and (word.isdigit() == False):        # (weight > 2000)
                     #word = str(word)        # Because it is unicode - special chars cannot be converted to ascii
                     words.append(word)      # TODO add only unique words
                     unique_words.add(word)
@@ -60,6 +63,27 @@ def train_lda():
     print(corpus[0])
     return models.ldamodel.LdaModel(corpus, num_topics=20, id2word = dictionary, passes=20)
 
+def represent_as_bow():
+    print("CBOW")
+    vectorizer = CountVectorizer(analyzer = "word",   \
+                                tokenizer = None,    \
+                                preprocessor = None, \
+                                stop_words = None,   \
+                                max_features = 5000)
+    flattened_examples = [val for sublist in examples for val in sublist]
+    print("All words count:", len(flattened_examples))
+    train_data_features = vectorizer.fit_transform(list(unique_words))
+    train_data_features = train_data_features.toarray()
+    print(train_data_features.shape, train_data_features[0])
+    vocab = vectorizer.get_feature_names()
+    print(vocab[:100])
+
+def load_word2vec_model(directory, filename):
+    model = Word2Vec.load_word2vec_format(os.path.join(directory, filename), binary=True)
+    # Print a wordsXfeatures matrrix shape
+    print(model.syn0.shape)
+    print(model["car"])
+
 
 
 
@@ -67,7 +91,7 @@ def train_lda():
 if __name__ == '__main__':
     # Data directory of train file
     data_directory = '/media/dmacjam/Data disc/Open data/TBIR/data_6stdpt/Features/Textual/'
-    read_textual_features(data_directory, 'train_data.txt', 3000)
+    read_textual_features(data_directory, 'train_data.txt', 1000)
     #print(images)
     print(len(examples))
     print(len(examples[0]), examples[0])
@@ -77,3 +101,6 @@ if __name__ == '__main__':
 
     #lda_model = train_lda()
     #print(lda_model.print_topics(num_topics=10))
+
+    represent_as_bow()
+    load_word2vec_model('/home/dmacjam/Word2Vec/','vectors.bin')
