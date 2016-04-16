edee1cc8092292884747c20b3cec50cc89e6e603
Jakub Macina <jakub.macina@gmail.com>
2016-04-16 19:13:16 +0200

    Add lda to function

--- main.py
+++ main.py
@@ -10,13 +10,19 @@ images = []
 examples = []
 unique_words = set()
 
-def read_textual_features(directory, filename):
+def read_textual_features(directory, filename, number_of_examples_to_load):
+    '''
+    Read and preprocess textual features.
+    :param directory:
+    :param filename:
+    :return:
+    '''
     global unique_words, examples
     p_stemmer = PorterStemmer()
     stop = stopwords.words('english')
     number_of_lines = 0
     with open(os.path.join(directory, filename)) as textual_file:
-        for i in range(0,100):
+        for i in range(0,number_of_examples_to_load):
         #for line in textual_file:
             line = textual_file.readline()
             words = []
@@ -28,7 +34,7 @@ def read_textual_features(directory, filename):
                 word = line_words[i+2].decode('utf-8')
                 word = p_stemmer.stem(word)
                 weight = int(line_words[i+3])
-                if (word not in stop) and (word.isdigit() == False) and (weight > 2000):
+                if (word not in stop) and (word.isdigit() == False):        # (weight > 2000)
                     #word = str(word)        # Because it is unicode - special chars cannot be converted to ascii
                     words.append(word)      # TODO add only unique words
                     unique_words.add(word)
@@ -36,20 +42,29 @@ def read_textual_features(directory, filename):
     print(number_of_lines)
     #words.shape = (number_of_lines, words.shape[0]/number_of_lines)
 
+def train_lda():
+    '''
+    LDA training by gensim.
+    :param corpus:
+    :return:
+    '''
+    dictionary = corpora.Dictionary(examples)
+    print(len(dictionary), dictionary.token2id)
+    corpus = [dictionary.doc2bow(text) for text in examples]
+    print(corpus[0])
+    return models.ldamodel.LdaModel(corpus, num_topics=20, id2word = dictionary, passes=20)
+
 
 # Main Functio
 if __name__ == '__main__':
     # Data directory of train file
     data_directory = '/media/dmacjam/Data disc/Open data/TBIR/data_6stdpt/Features/Textual/'
-    read_textual_features(data_directory, 'train_data.txt')
+    read_textual_features(data_directory, 'train_data.txt', 100)
     #print(images)
     print(len(examples))
     print(len(examples[0]), examples[0])
     #pprint(examples[:10])
     print(len(unique_words))
-    dictionary = corpora.Dictionary(examples)
-    print(len(dictionary), dictionary.token2id)
-    corpus = [dictionary.doc2bow(text) for text in examples]
-    print(corpus[0])
-    ldamodel = models.ldamodel.LdaModel(corpus, num_topics=20, id2word = dictionary, passes=20)
-    print(ldamodel.print_topics(num_topics=10))
+
+    lda_model = train_lda()
+    print(lda_model.print_topics(num_topics=10))
