dcb0465e2f97758cc41aafd718b270b830107534
Jakub Macina <jakub.macina@gmail.com>
2016-04-22 11:38:22 +0200

    Add DataManager with vocabulary and corpus

--- DataManager.py
+++ DataManager.py
@@ -1,14 +1,92 @@
 import os
+from nltk.corpus import stopwords
+from nltk.stem.porter import PorterStemmer
+from nltk.tokenize import word_tokenize
+
+websites_data_directory = '/media/dmacjam/Data disc/Open data/TBIR/data_6stdpt/Features/Textual/'
+websites_filename = 'train_data.txt'
+
 
 class DataManager(object):
+    text_examples = []
+    text_examples_weights = []
+    id2word = dict()
+    word2id = dict()
+    unique_words = set()
+    text_examples_img_ids = []
+    corpus = []
+
+    def __init__(self):
+        pass
 
     @staticmethod
-    def read_visual_file(directory, filename, number_of_examples_to_load):
-        with open(os.path.join(directory, filename)) as textual_file:
-             for i in range(0,number_of_examples_to_load):
+    def _readVisualFile(number_of_examples_to_load):
+        with open(os.path.join(websites_data_directory, websites_filename)) as textual_file:
+             for i in range(0, number_of_examples_to_load):
                  yield textual_file.readline()
 
-    @staticmethod
-    def create_vocabulary(directory, filename, number_of_examples_to_load):
-        # TODO presun sem tu metodu na nacitanie vocabulary
-        print("Abc")
+    def createVocabulary(self, number_of_examples_to_load):
+        '''
+        Read and preprocess textual features. Create vocabulary and corpus.
+        :param directory:
+        :param filename:
+        :param number_of_examples_to_load:
+        :return:
+        '''
+        p_stemmer = PorterStemmer()
+        stop = stopwords.words('english')
+        number_of_lines = 0
+        number_of_words = 0
+        unique_words_counter = 0
+        lines = DataManager._readVisualFile(number_of_examples_to_load)
+        for line in lines:
+            # For every line in document.
+            words = []
+            weights = []
+            corpus_line_dict = dict()
+            number_of_lines += 1
+            line_words = line.split()
+            self.text_examples_img_ids.append(line_words[0])
+            number_of_features = int(line_words[1])
+            for j in range(0, number_of_features, 2):
+                number_of_words += 1
+                word = line_words[j + 2].decode('utf-8')
+                # Tokenize word to delete apostrophe.
+                word = word_tokenize(word)[0]
+                # Stem the word.
+                word = p_stemmer.stem(word)
+                # Normalize weight
+                weight = float(line_words[j + 3]) / 100000
+                # TODO filter 2 char words, better filter digits e.g. 12th
+                if (word not in stop) and (word.isdigit() == False):  # (weight > 2000)
+                    if (word not in self.unique_words):
+                        # words.append(word)
+                        self.unique_words.add(word)
+                        self.id2word[unique_words_counter] = word
+                        self.word2id[word] = unique_words_counter
+                        corpus_line_dict[unique_words_counter] = weight
+                        unique_words_counter += 1
+                        # weights.append(weight)
+                    else:
+                        word_id = self.word2id[word]
+                        if word_id in corpus_line_dict:
+                            corpus_line_dict[word_id] += weight
+                        else:
+                            corpus_line_dict[word_id] = weight
+            # Create array of tuples (word_id, weight)
+            corpus_line = []
+            for key, value in corpus_line_dict.iteritems():
+                corpus_line.append( (key, value) )
+            self.corpus.append(corpus_line)
+            # Debug print
+            if ((number_of_lines + 1) % 4000 == 0):
+                print("Read %d of %d\n" % (number_of_lines + 1, number_of_examples_to_load))
+            #self.text_examples.append(words)
+            #self.text_examples_weights.append(weights)
+
+        print("Number of text examples read:", number_of_lines, len(self.text_examples))
+        print("Number of unique words:", len(self.unique_words), "from ", number_of_words)
+
+        print self.corpus[0]
+        print self.id2word[1]
+
--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import numpy as np
 import os
-from nltk.corpus import stopwords
-from nltk.stem.porter import PorterStemmer
+
 from gensim import corpora, models
 from pprint import pprint
 from sklearn.feature_extraction.text import CountVectorizer
@@ -9,61 +8,12 @@ import logging
 from Word2VecVectors import Word2VecProcessing
 from DataManager import DataManager
 
-# Set up gensim logging
-logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=logging.INFO)
-
-images = []
-examples = []
-examples_weights = []
-id2word = dict()
-unique_words = set()
 
 # Set up data directories
 word_to_vec_trained_model_data_directory = '/home/dmacjam/Word2Vec/'
-websites_data_directory = '/media/dmacjam/Data disc/Open data/TBIR/data_6stdpt/Features/Textual/'
 
-
-def readTextualFeatures(directory, filename, number_of_examples_to_load):
-    '''
-    Read and preprocess textual features. We cannot load it into numpy, because number of features is not same for
-    every example.
-    :param directory:
-    :param filename:
-    :return:
-    '''
-    global unique_words, examples, examples_weights
-    p_stemmer = PorterStemmer()
-    stop = stopwords.words('english')
-    number_of_lines = 0
-    number_of_words = 0
-    unique_words_counter = 0
-    lines = DataManager.read_visual_file(directory, filename, number_of_examples_to_load)
-    for line in lines:
-        words = []
-        weights = []
-        number_of_lines += 1
-        line_words = line.split()
-        images.append(line_words[0])
-        number_of_features = int(line_words[1])
-        for j in range(0, number_of_features, 2):
-            number_of_words += 1
-            word = line_words[j + 2].decode('utf-8')
-            word = p_stemmer.stem(word)
-            weight = float(line_words[j + 3]) / 100000
-            # TODO filter 2 char words, better filter digits e.g. 12th
-            if (word not in stop) and (word.isdigit() == False):  # (weight > 2000)
-                # word = str(word)        # Because it is unicode - special chars cannot be converted to ascii
-                if (word not in unique_words):
-                    words.append(word)
-                    unique_words.add(word)
-                    id2word[unique_words_counter] = word
-                    weights.append(weight)
-        if ((number_of_lines + 1) % 4000 == 0):
-            print("Read %d of %d\n" % (number_of_lines + 1, number_of_examples_to_load))
-        examples.append(words)
-        examples_weights.append(weights)
-    print("Number of text examples read:", number_of_lines, len(examples))
-    print("Number of unique words:", len(unique_words), "from ", number_of_words)
+# Set up gensim logging
+logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=logging.INFO)
 
 
 
@@ -115,7 +65,9 @@ def loadImageIds(directory, filename):
 if __name__ == '__main__':
 
 
-    readTextualFeatures(websites_data_directory, 'train_data.txt', 5000)
+    data_manager = DataManager()
+    data_manager.createVocabulary(1000)
+    #readTextualFeatures(websites_data_directory, 'train_data.txt', 5000)
     #print(images)
     #print(len(examples[0]), examples[0])
     #pprint(examples[:10])
